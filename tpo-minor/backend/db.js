1db145428680b7529db453c8537e31d6d31b7ea4
RAVI10121 <105443219+RAVI10121@users.noreply.github.com>
2023-03-31 16:55:00 +0530

    Update db.js

--- tpo-minor/backend/db.js
+++ tpo-minor/backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const URI =
   // "mongodb://localhost:27017/tpo?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
-  "mongodb+srv://pranav:Pg07%40mongodbatlas@cluster0.1y6tm.mongodb.net/tpo?retryWrites=true&w=majority"
+  "mongodb+srv://rkravi1012:raviahirwar@cluster0.u7afg0v.mongodb.net/?retryWrites=true&w=majorit"
 const connect = () => {
   mongoose.connect(URI, () => {
     console.log("Connect ho gya bhayaji !!");
